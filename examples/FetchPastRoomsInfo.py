74f155a5b9f9c4c23c0cfd8ed55716eb7808b773
Mohammadjavad Hoseini <mj.hossini.dev@gmail.com>
2025-07-14 08:37:01 +0000

    feat: add examples for all routes

---
+++ examples/FetchPastRoomsInfo.py
@@ -0,0 +1,20 @@
+from src.PlugNMeet import PlugNMeet
+from src.schemas.requests import FetchPastRoomsInfoRequest
+from src.schemas.data import OrderBy
+
+async def main():
+    meet_platform = PlugNMeet(base_url="http://127.0.0.1:8080", api_key="plugnmeet", api_secret="zumyyYWqv7KR2kUqvYdq4z4sXg7XTBD2ljT6")
+    status, result = await meet_platform.fetchPastRoomsInfo(
+        data=FetchPastRoomsInfoRequest(
+                room_ids=["fff44422_werwe"],
+                from_=0,
+                limit=3,
+                order_by=OrderBy.ASC.value
+            )
+        )
+    print(f"status: {status}")
+    print(f"result: {result}")
+    
+if __name__ == "__main__":
+    from asyncio import run
+    run(main())
